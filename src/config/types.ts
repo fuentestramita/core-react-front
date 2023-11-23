8cb84a770c349e2918a283e5a8b8bd7b4a900f4f
Stewart Granger <stewart.grangerf@gmail.com>
2023-11-22 23:16:15 -0300

    [Layout] Fix react node type

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,3 +1,1 @@
-import { JSXElementConstructor, ReactElement } from "react";
-
-export type ChildrenType = ReactElement<any, string | JSXElementConstructor<any>>;
+export type ChildrenType = React.ReactNode;
